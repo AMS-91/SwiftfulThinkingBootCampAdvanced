35d4d955f7b4eda4c62d7b77e5b03f5811e00830
DEVready <amsmutu@gmail.com>
2022-04-24 19:43:10 +0900

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by 안민수 on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
